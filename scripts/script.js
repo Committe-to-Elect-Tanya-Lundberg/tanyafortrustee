47e1463c93c00f12b45fd887175817c9b78b99eb
Jeff Lundberg <git@jefflundberg.com>
2020-08-23 13:52:29 -0400

    animate issues images

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,9 @@
 const nav = document.querySelector("nav")
 const navObserver = document.querySelector(".nav-observer")
 
+const callout = document.querySelectorAll(".callout")
+
+
 const createObserver = () => {
     const options = {
         root: null,
@@ -17,6 +20,23 @@ const createObserver = () => {
     }, options)
 
     observer.observe(navObserver)
+
+    let calloutObserver = new IntersectionObserver(
+        e => {
+            if (e[0].isIntersecting) {
+                e[0].target.classList.add("visible")
+            } else {
+                // e[0].target.classList.remove("visible")
+            }
+        },
+        {
+            root: null,
+            rootMargin: "0px",
+            threshold: .3,
+        }
+    )
+
+    callout.forEach(e => calloutObserver.observe(e))
 }
 
 window.addEventListener(
